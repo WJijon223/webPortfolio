66ee8bf4d9de39bc88f4e62f40eca1b3554e5aa6
WJijon223 <williamjijon223@gmail.com>
2024-11-02 00:54:58 -0400

    added fisherman game section, email form, and links to socials

---
+++ emailForm.js
@@ -0,0 +1,22 @@
+"use strict";
+const btn = document.getElementById("button");
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_qh1vm0m";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
