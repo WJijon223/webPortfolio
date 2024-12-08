37d089554d2ceabd99ea7205c04d60da1a8d66a2
WJijon223 <williamjijon223@gmail.com>
2024-12-08 12:06:08 -0500

    added typing.js and tweaked css for small screens

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+import Typed from "./node_modules/typed.js/dist/typed.module.js";
+
+const options = {
+  strings: [
+    "Computer Science Student",
+    "Aspiring Developer",
+    "Technology Enthusiast",
+  ],
+  typeSpeed: 30, //typing speed in ms
+  backSpeed: 30, //deletion speed in ms
+  backDelay: 1350, //delay before deleting in ms
+  loop: true, //keep the animation looping
+  showCursor: false,
+};
+
+var typed = new Typed("#typing-text", options);
